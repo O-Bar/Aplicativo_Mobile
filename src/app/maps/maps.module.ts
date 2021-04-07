9ad819d028fea7facd1b39726ce9348626a2e0d6
amoreto <amoreto@indracompany.com>
2021-04-07 16:44:46 -0300

    modo de mapa 2

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { GoogleMapsModule } from '@angular/google-maps';
 import { IonicModule } from '@ionic/angular';
-
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 import { MapsPageRoutingModule } from './maps-routing.module';
 
 import { MapsPage } from './maps.page';
@@ -13,8 +13,23 @@ import { MapsPage } from './maps.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    MapsPageRoutingModule
+    MapsPageRoutingModule,
+    GoogleMapsModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
   ],
   declarations: [MapsPage]
 })
-export class MapsPageModule {}
+export class MapsPageModule {
+  center: google.maps.LatLngLiteral = {lat: 24, lng: 12};
+  zoom = 4;
+  display: google.maps.LatLngLiteral;
+
+  moveMap(event: google.maps.MapMouseEvent) {
+    this.center = (event.latLng.toJSON());
+  }
+
+  move(event: google.maps.MapMouseEvent) {
+    this.display = event.latLng.toJSON();
+  }
+}
--- src/app/maps/maps.page.ts
+++ src/app/maps/maps.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, AfterViewInit, ElementRef, ViewChild  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-maps',
@@ -7,56 +10,18 @@ import { Component, OnInit, AfterViewInit, ElementRef, ViewChild  } from '@angul
 })
 export class MapsPage implements OnInit {
 
-  @ViewChild('mapRef', {static: true }) mapElement: ElementRef;
+  apiLoaded: Observable<boolean>;
 
-  constructor() { }
+  constructor(httpClient: HttpClient) { 
+    this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyAV8JNLGGLWj9V77RPLh4GQtz2z7oeNDhI', 'callback')
+        .pipe(
+          map(() => true),
+          catchError(() => of(false)),
+        );
+        
+  }
 
   ngOnInit() {
-    this.renderMap();
-  }
-  loadMap = () => {
-    var map = new window['google'].maps.Map(this.mapElement.nativeElement, {
-      center: {lat: 24.5373, lng: 81.3042},
-      zoom: 8
-    });
-    var marker = new window['google'].maps.Marker({
-      position: {lat: 24.5373, lng: 81.3042},
-      map: map,
-      title: 'Hello World!',
-      draggable: true,
-      animation: window['google'].maps.Animation.DROP,
-    });
-    var contentString = '<div id="content">'+
-    '<div id="siteNotice">'+
-    '</div>'+
-    '<h3 id="thirdHeading" class="thirdHeading">tutsmake.com</h3>'+
-    '<div id="bodyContent">'+
-    '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>'+
-    '</div>'+
-    '</div>';
-  
-    var infowindow = new window['google'].maps.InfoWindow({
-      content: contentString
-    });
-    marker.addListener('click', function() {
-      infowindow.open(map, marker);
-    });
-
-  }
-  renderMap() {
-      
-    window['initMap'] = () => {
-      this.loadMap();      
-    }
-    if(!window.document.getElementById('google-map-script')) {
-      var s = window.document.createElement("script");
-      s.id = "google-map-script";
-      s.type = "text/javascript";
-      s.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAV8JNLGGLWj9V77RPLh4GQtz2z7oeNDhI&callback=initMap";
-        
-      window.document.body.appendChild(s);
-    } else {
-      this.loadMap();
-    }
+   
   }
 }
