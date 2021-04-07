7aeb526c796cca51a8bb81f06d71df353651794e
amoreto <amoreto@indracompany.com>
2021-04-07 15:55:08 -0300

    Mapa funcional

--- src/app/maps/maps.page.ts
+++ src/app/maps/maps.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild  } from '@angular/core';
 
 @Component({
   selector: 'app-maps',
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapsPage implements OnInit {
 
+  @ViewChild('mapRef', {static: true }) mapElement: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+    this.renderMap();
   }
+  loadMap = () => {
+    var map = new window['google'].maps.Map(this.mapElement.nativeElement, {
+      center: {lat: 24.5373, lng: 81.3042},
+      zoom: 8
+    });
+    var marker = new window['google'].maps.Marker({
+      position: {lat: 24.5373, lng: 81.3042},
+      map: map,
+      title: 'Hello World!',
+      draggable: true,
+      animation: window['google'].maps.Animation.DROP,
+    });
+    var contentString = '<div id="content">'+
+    '<div id="siteNotice">'+
+    '</div>'+
+    '<h3 id="thirdHeading" class="thirdHeading">tutsmake.com</h3>'+
+    '<div id="bodyContent">'+
+    '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>'+
+    '</div>'+
+    '</div>';
+  
+    var infowindow = new window['google'].maps.InfoWindow({
+      content: contentString
+    });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
 
+  }
+  renderMap() {
+      
+    window['initMap'] = () => {
+      this.loadMap();      
+    }
+    if(!window.document.getElementById('google-map-script')) {
+      var s = window.document.createElement("script");
+      s.id = "google-map-script";
+      s.type = "text/javascript";
+      s.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAV8JNLGGLWj9V77RPLh4GQtz2z7oeNDhI&callback=initMap";
+        
+      window.document.body.appendChild(s);
+    } else {
+      this.loadMap();
+    }
+  }
 }
