1c12371ca5ec745f9308da57c23f03bf1c2574f4
AugustoMoret <gutomoreto@gmail.com>
2021-04-06 23:54:58 -0300

    nova pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'maps',
+    loadChildren: () => import('./maps/maps.module').then( m => m.MapsPageModule)
   },
+
 ];
 
 @NgModule({
