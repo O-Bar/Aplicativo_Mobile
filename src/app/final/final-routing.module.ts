7c90b16fef8a22e2f869591f2a740801ee1b4981
gabistavik <stavik628@gmail.com>
2021-04-11 18:10:40 -0300

    Página de ajuda e sobre nos

--- src/app/final/final-routing.module.ts
+++ src/app/final/final-routing.module.ts
@@ -6,9 +6,20 @@ import { FinalPage } from './final.page';
 const routes: Routes = [
   {
     path: '',
-    component: FinalPage
-  }
-];
+    component: FinalPage,
+    children:[
+      {
+        path: 'ajuda',
+        loadChildren: () => import('./ajuda/ajuda.module').then( m => m.AjudaPageModule)
+      },
+      {
+        path: 'sobre',
+        loadChildren: () => import('./sobre/sobre.module').then( m => m.SobrePageModule)
+      }
+    ]
+  }]
+ 
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
