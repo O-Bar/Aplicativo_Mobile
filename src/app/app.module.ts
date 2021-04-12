b22201fa21538388fd1861fc9a913b88edd2a9b0
heskiian <heskiian@gmail.com>
2021-04-12 17:47:56 -0300

    FirstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,11 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { DemoComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -13,5 +20,19 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgbModalModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+
+    }),
+    BrowserModule
+  ],
+  declarations: [DemoComponent],
+  exports: [DemoComponent],
+  bootstrap: [DemoComponent]
 })
-export class AppModule {}
