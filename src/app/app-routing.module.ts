818c0d04f0fa679d68dbb9150cc8700f90a65ae2
heskiian <heskiian@gmail.com>
2021-04-12 18:37:46 -0300

    teste de importação de arquivos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'calendario',
+    loadChildren: () => import('./calendario/calendario.module').then( m => m.CalendarioPageModule)
   },
+
 ];
 
 @NgModule({
