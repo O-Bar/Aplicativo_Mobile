0999c01c31e6d32176f84fb378346556e2f9d87e
heskiian <heskiian@gmail.com>
2021-06-03 20:37:08 -0300

    fix auth e redirect

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { AngularFireAuthGuard, redirectLoggedInTo } from '@angular/fire/auth-guard'
 import { LoginPage } from './login.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: LoginPage
+    component: LoginPage,
+    canActivate: [AngularFireAuthGuard],
+    data : { authGuardPipe: () => redirectLoggedInTo('/home/pagina-inicial')}
   }
 ];
 
