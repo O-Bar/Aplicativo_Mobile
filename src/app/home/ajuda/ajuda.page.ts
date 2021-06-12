a5d0a8e73831f9d9b9569174e87e0b22729fc26d
gabistavik <stavik628@gmail.com>
2021-06-12 19:30:33 -0300

    expandable list

--- src/app/home/ajuda/ajuda.page.ts
+++ src/app/home/ajuda/ajuda.page.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ajuda',
   templateUrl: './ajuda.page.html',
   styleUrls: ['./ajuda.page.scss'],
 })
-export class AjudaPage implements OnInit {
+export class AjudaPage  {
+  public items: any = [];
+  public items2: any = []; 
+  public items3: any = [];
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor() { 
+    this.items = [
+      { expanded: false }
+    ];
+    this.items2 = [
+      { expanded: false }
+    ];
+    this.items3 = [
+      { expanded: false }
+    ];  
   }
 
+  expandItem(item): void {
+    if (item.expanded) {
+      item.expanded = false;
+    } else {
+      this.items.map(listItem => {
+        if (item == listItem) {
+          listItem.expanded = !listItem.expanded;
+        } else {
+          listItem.expanded = false;
+        }
+        return listItem;
+      });
+    }
+  }
+  expandItem2(item2): void {
+    if (item2.expanded) {
+      item2.expanded = false;
+    } else {
+      this.items2.map(listItem2 => {
+        if (item2 == listItem2) {
+          listItem2.expanded = !listItem2.expanded;
+        } else {
+          listItem2.expanded = false;
+        }
+        return listItem2;
+      });
+    }
+  }
+  expandItem3(item3): void {
+    if (item3.expanded) {
+      item3.expanded = false;
+    } else {
+      this.items2.map(listItem3 => {
+        if (item3 == listItem3) {
+          listItem3.expanded = !listItem3.expanded;
+        } else {
+          listItem3.expanded = false;
+        }
+        return listItem3;
+      });
+    }
+  }
 }
